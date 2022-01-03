015e500eda68529f7b023fba0ba5ad530e51f0ad
sareum <rkdtkfma@gmail.com>
2022-01-03 21:30:24 +0900

    Add foryouPage mockup and fix foryoucard component

--- client/src/page/forYou.jsx
+++ client/src/page/forYou.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import style from "./forYou.module.css";
 import ForYouCard from "../components/forYouCard";
+// import dummy2 from "../components/dummy2";
 
 const ForYou = () => {
   return (
-    <div>
-      <ForYouCard />
+    <div className={style.container}>
+      <div className={style.selectbox}>
+        <select
+          height="long"
+          initValue="ALL"
+          className={style.category}
+        >
+          <option value="ALL">ALL</option>
+          <option value="동기부여">동기부여를 받고 싶다면 ?</option>
+          <option value="도전">도전하고 싶은 나에게</option>
+          <option value="멘토">현재 나의 상황에 멘토를 원하시나요 ?</option>
+          <option value="편안함">마음속 편안함을 찾는다면 ?</option>
+          <option value="웃음">생각없이 웃고 싶다면 ?</option>
+          <option value="눈물">오늘 한 없이 눈물을 쏟고 싶다면 ?</option>
+          <option value="백색소리">백색소리</option>
+        </select>
+        <button className={style.btn}>리스트 작성하기</button>
+      </div>
+      <div className={style.cardContainer}>
+        <ForYouCard />
+        <ForYouCard />
+        <ForYouCard />
+        <ForYouCard />
+        <ForYouCard />
+        <ForYouCard />
+        <ForYouCard />
+        <ForYouCard />
+      </div>
     </div>
   );
 };
