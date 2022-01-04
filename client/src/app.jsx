fb8c89858551a12790d379556d3bf90e3a75b190
sareum <rkdtkfma@gmail.com>
2022-01-04 23:05:40 +0900

    Add navigate from foryoupage to foryouwriting

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -9,6 +9,7 @@ import ForYou from "./page/forYou";
 import Contents from "./page/contents";
 import Login from "./page/login";
 import ContentsModal from "./components/contentsModal";
+import ForYouWriting from "./page/forYouWriting";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/foryou" element={<ForYou />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/foryouwriting" element={<ForYouWriting />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- client/src/page/forYou.jsx
+++ client/src/page/forYou.jsx
@@ -1,55 +1,60 @@
 import React, { useRef } from "react";
 import style from "./forYou.module.css";
 import ForYouCard from "../components/forYouCard";
-import ForYouWriting from "./forYouWriting";
+import { useNavigate } from "react-router-dom";
 import dummy2 from '../dummy/dummy2';
 
 const ForYou = () => {
-  console.log(dummy2)
-  const select = useRef();
-  const { image } = dummy2[0];
+    const navigate = useNavigate();
 
-  // const select_1_category = dummy2.filter((el) => {
-  //   let category = el.category;
-  //   for (let i = 0; i < category.length; i++) {
-  //     if (category[i] === select) {
-  //       console.log("카테고리", category[i]);
-  //       return category[i];
-  //     } else if (select === "ALL") {
-  //       return category[i];
-  //     }
-  //   }
-  // });
+    const handleClick = () => {
+        navigate("/foryouwriting");
+    };
 
-  return (
-    <div className={style.container}>
-      {/* <div className={style.selectbox}>
-        <select
-          className={style.category}
-          ref={select}
-        >
-          <option value="ALL">ALL</option>
-          <option value="동기부여">동기부여를 받고 싶다면 ?</option>
-          <option value="도전">도전하고 싶은 나에게</option>
-          <option value="멘토">현재 나의 상황에 멘토를 원하시나요 ?</option>
-          <option value="편안함">마음속 편안함을 찾는다면 ?</option>
-          <option value="웃음">생각없이 웃고 싶다면 ?</option>
-          <option value="눈물">오늘 한 없이 눈물을 쏟고 싶다면 ?</option>
-          <option value="백색소리">백색소리</option>
-        </select>
-        <button className={style.btn}>리스트 작성하기</button>
-      </div>
-      <div className={style.cardContainer}>
-        <ForYouCard img={image} />
-        <ForYouCard />
-        <ForYouCard />
-        <ForYouCard />
-        <ForYouCard />
-        <ForYouCard />
-      </div> */}
-      <ForYouWriting />
-    </div>
-  );
+    console.log(dummy2)
+    const select = useRef();
+    const { image } = dummy2[0];
+
+    // const select_1_category = dummy2.filter((el) => {
+    //   let category = el.category;
+    //   for (let i = 0; i < category.length; i++) {
+    //     if (category[i] === select) {
+    //       console.log("카테고리", category[i]);
+    //       return category[i];
+    //     } else if (select === "ALL") {
+    //       return category[i];
+    //     }
+    //   }
+    // });
+
+    return (
+        <div className={style.container}>
+            <div className={style.selectbox}>
+                <select
+                    className={style.category}
+                    ref={select}
+                >
+                    <option value="ALL">ALL</option>
+                    <option value="동기부여">동기부여를 받고 싶다면 ?</option>
+                    <option value="도전">도전하고 싶은 나에게</option>
+                    <option value="멘토">현재 나의 상황에 멘토를 원하시나요 ?</option>
+                    <option value="편안함">마음속 편안함을 찾는다면 ?</option>
+                    <option value="웃음">생각없이 웃고 싶다면 ?</option>
+                    <option value="눈물">오늘 한 없이 눈물을 쏟고 싶다면 ?</option>
+                    <option value="백색소리">백색소리</option>
+                </select>
+                <button className={style.btn} onClick={handleClick}>리스트 작성하기</button>
+            </div>
+            <div className={style.cardContainer}>
+                <ForYouCard img={image} />
+                <ForYouCard />
+                <ForYouCard />
+                <ForYouCard />
+                <ForYouCard />
+                <ForYouCard />
+            </div>
+        </div>
+    );
 };
 
 export default ForYou;
