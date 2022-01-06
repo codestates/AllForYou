9e45d7cb7b724d3ec5cf3535da9ff06abe059e8c
LeeJaeYoung <87467464+dlwodud9529@users.noreply.github.com>
2022-01-06 13:52:09 +0900

    set server coding

---
+++ server/controllers/contents/contentRouter.js
@@ -0,0 +1,5 @@
+module.exports = {
+    list: require("./list"),
+    read: require("./read"),
+    like: require("./like")
+  };
---
+++ server/models/likes.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class likes extends Model {
+    static associate(models) {
+    /**
+    * belongsTo : 1:1 하나만 가질 수 있다.
+    */
+      likes.belongsTo(models.users, { foreignKey: { name: "user_id", allowNull: false }, onDelete: "CASCADE" });
+      likes.belongsTo(models.reviews, { foreignKey: { name: "review_id", allowNull: false }, onDelete: "CASCADE" });
+      likes.belongsTo(models.contents, { foreignKey: { name: "content_id", allowNull: false }, onDelete: "CASCADE" });
+    }
+  };
+  likes.init({
+    user_id: DataTypes.INTEGER,
+    review_id: DataTypes.INTEGER,
+    content_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'likes',
+  });
+  return likes;
+};
