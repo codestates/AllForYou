2ee193deb6d7197975c3fea410738d3ef565618d
Hong <gg9297@gmail.com>
2022-01-06 11:51:16 +0900

    Add client mypageUpdate data ver2

--- client/src/components/mypageUpdate.jsx
+++ client/src/components/mypageUpdate.jsx
@@ -1,3 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import style from "./mypageProfilebox.module.css";
 import axios from 'axios';
+
+function MyPgaeUpdate() {
+
+    return (
+        <div>
+            회원정보 수정
+        </div>
+    );
+};
+
+export default MyPgaeUpdate;
