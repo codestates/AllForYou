115b23c54a5a38911ad5400fd576feb40fd6c74c
LeeJaeYoung <87467464+dlwodud9529@users.noreply.github.com>
2022-01-06 13:58:34 +0900

    set 작업할 영역

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,24 +5,25 @@ const userRouter = require("./users/userRouter");
 const reviewRouter = require("./reviews/reviewRouter");
 
 //users
-router.post("/users/signup", userRouter.signUp); // 회원가입
-router.post("/users/signin", userRouter.signIn); // 로그인
-router.post("/users/signout", auth.accessToken, userRouter.signOut); // 로그아웃
+router.post("/users/signup", userRouter.signUp); // 회원가입(완료)
+router.post("/users/signin", userRouter.signIn); // 로그인(완료)
+router.post("/users/signout", auth.accessToken, userRouter.signOut); // 로그아웃(완료)
 
+//oauth(세환 진행중)
 router.get("/users/kakao", userRouter.kakao); // kakao 토큰 받기
 router.get("/users/kakaoCallback", userRouter.kakaoCallback); // kakao 로그인
 router.get("/users/google", userRouter.google); // google 토큰 받기
 router.get("/users/googleCallback", userRouter.googleCallback); // google 로그인
 
 //mypage
-router.get("/users/mypage", auth.accessToken, userRouter.userInfo); // 유저정보 확인, 좋아요 누른 컨텐츠 5개, 내가쓴 리뷰글 5개
-router.patch("/users/mypage", auth.accessToken, userRouter.modifyUser); // 회원정보 수정
-router.delete("/users/mypage", auth.accessToken, userRouter.withdrawal); // 회원탈퇴
+router.get("/users/mypage", auth.accessToken, userRouter.userInfo); // 유저정보 확인, 좋아요 누른 컨텐츠 5개, 내가쓴 리뷰글 5개(완료)
+router.patch("/users/mypage", auth.accessToken, userRouter.modifyUser); // 회원정보 수정(완료)
+router.delete("/users/mypage", auth.accessToken, userRouter.withdrawal); // 회원탈퇴(완료)
 
-router.get("/users/mypage/myLike", auth.accessToken, userRouter.myLike); // 내가 좋아요 누른 컨텐츠 more
-router.get("/users/mypage/myReview", auth.accessToken, userRouter.myReview); // 내가쓴 리뷰글 more
+router.get("/users/mypage/myLike", auth.accessToken, userRouter.myLike); // 내가 좋아요 누른 컨텐츠 more(완료)
+router.get("/users/mypage/myReview", auth.accessToken, userRouter.myReview); // 내가쓴 리뷰글 more(완료)
 
-//reviews
+//reviews(재영 진행중)
 router.get("/reviews", reviewRouter.reviewList); // 리뷰 전체 불러오기
 router.get("/reviews/:postId", reviewRouter.reviewPick); // 리뷰 하나 불러오기
 
@@ -35,7 +36,7 @@ router.post("/reviews/:postId/comment", auth.accessToken, null) // 리뷰에 댓
 router.delete("/reviews/:postId/:commentId", auth.accessToken, null); // 리뷰 댓글 지우기
 router.delete("/reviews/:postId/:likeId", auth.accessToken, null); // 리뷰 좋아요 지우기
 
-//contents
+//contents(세환 진행중)
 router.get("/contents", null); // 컨텐츠 전체 불러오기
 router.get("/contents/:category", null); // 컨텐츠 카테고리 필터링
 router.get("/contents/:category/:type", null); // 컨텐츠 카테고리&타입 필터링
