0a3a40a7a63c0c755c6e9513e1e87c8b3de7d1d3
Hong <gg9297@gmail.com>
2022-01-10 18:35:23 +0900

    Fix commit error data

--- server/controllers/contents/contentRouter.js
+++ server/controllers/contents/contentRouter.js
@@ -1,15 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
     listAll: require("./contentsListAll"),
-=======
-    list: require("./contentsLikeList"),
->>>>>>> origin/dev
     firstfilter: require("./contentsFirstFilter"),
     secondlikefilter: require("./contentsLikeFilter"),
     seconddatefilter: require("./contentsDateFilter"),
     detail: require("./contentsDetail")
-<<<<<<< HEAD
 };
-=======
-  };
->>>>>>> origin/dev
