94aa13da26d77b61174103fa0d69809ee52b6c44
LeeJaeYoung <87467464+dlwodud9529@users.noreply.github.com>
2022-01-12 10:25:55 +0900

    fix server code 2

--- server/controllers/contents/contentRouter.js
+++ server/controllers/contents/contentRouter.js
@@ -1,11 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    // list: require("./contentsList"),
-    // firstfilter: require("./contentsFirstFilter"),
-    // secondfilter: require("./contentsSecondFilter"),
-    // detail: require("./contentsDetail")
-  };
-=======
     listAll: require("./contentAll"),
     firstfilter: require("./contentsFirstFilter"),
     secondlikefilter: require("./contentsLikeFilter"),
@@ -13,4 +6,3 @@ module.exports = {
     detail: require("./contentsDetail"),
     search: require("./contentSearch")
 };
->>>>>>> afbe23185f55b8c1f19d8115b719c46df361a03d
