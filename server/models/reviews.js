fab1a0f55034210a841ac7f4f23eca08382addd7
Hong <gg9297@gmail.com>
2022-01-13 10:07:14 +0900

    Fix server models data

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -18,7 +18,6 @@ module.exports = (sequelize, DataTypes) => {
     text: DataTypes.TEXT,
     image: DataTypes.STRING
   }, {
-    timestamps: false,
     sequelize,
     modelName: 'reviews',
   });
