a3806771fbcba2ae13c15d5c0631f2582a2ca8e7
LeeJaeYoung <87467464+dlwodud9529@users.noreply.github.com>
2022-01-16 12:45:30 +0900

    fix server code

--- server/controllers/reviews/reviewWrite.js
+++ server/controllers/reviews/reviewWrite.js
@@ -3,7 +3,6 @@ const { reviews, reviews_contents } = require("../../models");
 module.exports = async (req, res) => {
   const user_id = req.cookies.id;
   const { category, title, text, content_id } = req.body; // 컨텐츠 데이터 id를 받아온다.
-  const image = req.file.location;
 
   try{
     // 리뷰데이터 생성
@@ -11,10 +10,13 @@ module.exports = async (req, res) => {
       user_id,
       category,
       title,
-      text,
-      image
+      text
     })
 
+    if(req.file) {
+      await reviews.update({ image: req.file.location }, { where: { id: rewiewData.id } })
+    }
+    
     // 받아온 id값을 이용하여 테이블 생성
     content_id.map(async (el) => {
       await reviews_contents.create({ 
@@ -22,6 +24,7 @@ module.exports = async (req, res) => {
         content_id: el
       })
     })
+  
     return res.status(201).json({ data: rewiewData.id, message: "리뷰 작성완료." });
   }
   catch(err) {
