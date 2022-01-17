983183a2d486ae8f35674fe337d955684db90c44
NAM TAE SIK <tesnam1216@gmail.com>
2022-01-17 09:49:52 +0900

    landing page design

--- client/src/page/mainPage.jsx
+++ client/src/page/mainPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+import LandingPage_1 from "../page/landingPage/landingPage_1";
+import LandingPage_2 from "../page/landingPage/landingPage_2";
+
 const MainPage = () => {
   const navigate = useNavigate();
 
@@ -9,8 +12,10 @@ const MainPage = () => {
   };
   return (
     <div>
+      <LandingPage_1 />
+      <LandingPage_2 />
       <h1>메인페이지</h1>
-      <h2>메인페이지 2</h2>
+      {/* <h2>메인페이지 2</h2> */}
       <button onClick={handleClick}>콘텐츠로 가즈아</button>
     </div>
   );
