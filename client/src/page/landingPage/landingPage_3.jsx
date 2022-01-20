167d14d5880b94553e10ebe9b95b123f0eebbb28
sareum <rkdtkfma@gmail.com>
2022-01-20 14:25:28 +0900

    Add landingpage4 layout

--- client/src/page/landingPage/landingPage_3.jsx
+++ client/src/page/landingPage/landingPage_3.jsx
@@ -1,12 +1,12 @@
 import React, { useRef, useState, useEffect } from "react";
 import style from "./landingPage_3.module.css";
 import Carousel from '../../components/carousel';
-import useScrollCount from '../../components/useScrollCount'
+import useScrollCount from '../../hooks/useScrollCount'
 
 const LandingPage_3 = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-	const countUserNumber = useScrollCount({ end: 1853, start: 540, duration: 80 });
-	const countContentsNumber = useScrollCount({ end: 1000, start: 55, duration: 50 });
+	const [scrollPosition, setScrollPosition] = useState(0);
+	const countUserNumber = useScrollCount({ end: 1853, start: 1085, duration: 30 });
+	const countContentsNumber = useScrollCount({ end: 1000, start: 689, duration: 20 });
 
 	const onScroll = () => {
 		setScrollPosition(window.pageYOffset);
@@ -18,15 +18,15 @@ const LandingPage_3 = () => {
 		return () => window.removeEventListener('scroll', onScroll);
 	}, []);
 
-  return (
+	return (
     <div className={style.container}>
 			<div className={style.content}>
 				<div className={style.countMessage}>
             <div className={style.countBox}>
-              <div className={style.contentsCounter} {...countContentsNumber} />
-              <div className={style.contentsText}>개 이상의 컨텐츠를</div>
-              <div className={style.userCounter} {...countUserNumber} />
-              <div className={style.userText}>명의 유저들의</div>
+				<div className={style.contentsCounter} {...countContentsNumber} />
+				<div className={style.contentsText}>개 이상의 컨텐츠를</div>
+				<div className={style.userCounter} {...countUserNumber} />
+				<div className={style.userText}>명의 유저들의</div>
             </div>
 					<div className={style.text}>경험을 담아 추천하는 리스트를 확인해보세요!</div>
 				</div>
@@ -35,7 +35,7 @@ const LandingPage_3 = () => {
 				<Carousel />
 			</div>
 		</div>
-  );
+	);
 };
 
 export default LandingPage_3;
