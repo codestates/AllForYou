652d8785f4cab2e9ddd9e06f1b8f4ad9f1614c4f
LeeJaeYoung <87467464+dlwodud9529@users.noreply.github.com>
2022-01-21 09:42:35 +0900

    Delete server directory

--- server/controllers/contents/contentRouter.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    listAll: require("./contentsListAll"),
-    firstfilter: require("./contentsFirstFilter"),
-    secondlikefilter: require("./contentsLikeFilter"),
-    seconddatefilter: require("./contentsDateFilter"),
-    detail: require("./contentsDetail"),
-    search: require("./contentsSearch")
-};
