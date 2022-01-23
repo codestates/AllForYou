577a4a204dcd88092e8fd82418d22e78fde6d59c
Hong <gg9297@gmail.com>
2022-01-24 02:40:17 +0900

    Add server/client pagination

--- client/src/page/myReviewsDetail.jsx
+++ client/src/page/myReviewsDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import style from "./myReviewsDetail.module.css"
+import PageNationReviewButton from "../components/paginationReviewButton"
 import axios from "axios";
 import { useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
@@ -9,24 +10,127 @@ const MyReviewsDetail = () => {
     const { nickname } = useSelector((state) => state.loginReducer);
     const { profileImage } = useSelector((state) => state.loginReducer);
     
-    const [filterData, setFilterData] = useState(null)
+    const [isActive, setActive] = useState([]);
+    const [postItems, setPostItems] = useState([]);
+    const [postPages, setPostPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [showPostPages, setShowPostPages] = useState(0);
+    const [querys, setQuerys] = useState({
+        offset: 1,
+        limit: 10,
+    });
 
-    const handleReviewsDetail = () => {
+    const hadlePages = () => {
         axios
             .get(`${process.env.REACT_APP_SERVER_URL}/users/mypage/myReview`)
             .then((res) => {
-                const reviewData = res.data.data
-                setFilterData(reviewData)
-        })
+                const { row, count } = res.data.data;
+
+                let pageCount = Math.ceil(count / querys.limit);
+                setPostPages(pageCount);
+                const newPostPages = new Array(pageCount).fill(false).map((el, idx) => {
+                    if (idx === 0) return !el;
+                    return el;
+                });
+                setActive(newPostPages);
+                setPostItems(row);
+            }).catch ((err) => {
+                console.log(err);
+            })
     }
-        
+
+    const getPostsQuerys = async (newQuerys) => {
+        const keys = Object.keys(newQuerys);
+        const values = Object.values(newQuerys);
+    
+        const string = keys.reduce((a, c, idx) => {
+            if (values[idx]) {
+                const query = keys[idx] + '=' + values[idx] + '&';
+                return a + query;
+            } else {
+                return a;
+            }
+        }, '');
+    
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/users/mypage/myReview?${string}`);
+            const { row, count, page } = res.data.posts;
+
+            pageButtonCurrent(page - 1);
+            let pageCount = Math.ceil(count / querys.limit);
+            setPostPages(pageCount);
+            setPostItems(row);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    
+    const pageNumberOnclickFn = (e) => {
+        const newQuerys = {
+            ...querys,
+            offset: Number(e.target.innerText)
+        };
+        setQuerys(newQuerys);
+        getPostsQuerys(newQuerys);
+    };
+    
+    const pageArrowOnclickFn = (e) => {
+        const target = e.target.name;
+        let NextOffset = '';
+    
+        if (target === 'left') {
+            NextOffset = querys.offset > 1 ? querys.offset - 1 : 1;
+        } else {
+            NextOffset =
+            querys.offset + 1 > postPages ? postPages : querys.offset + 1;
+        }
+    
+        const newQuerys = {
+            ...querys,
+            offset: NextOffset
+        };
+    
+        setQuerys(newQuerys);
+        // 여기서 GET 요청 후 postPages 업데이트 되면서 useEffect 실행
+        getPostsQuerys(newQuerys);
+        setCurrentPage(newQuerys.offset);
+    };
+    
+    const pageButtonCurrent = (index) => {
+        const newActive = new Array(postPages).fill(false);
+        newActive[index] = !newActive[index];
+        setActive(newActive);
+    };
+
     useEffect(() => {
-        handleReviewsDetail();
+    hadlePages();
     }, []);
+
+    useEffect(() => {
+    pageButtonCurrent(0);
+    }, [postPages]);
+
+    useEffect(() => {
+    if (currentPage <= 5) {
+        setShowPostPages(0);
+        } else {
+            // current page 5의 배수로 증가
+            if ((currentPage - 1) % 5 === 0 || currentPage % 5 === 0) {
+                if (currentPage % 5 === 0) {
+                // arrow left
+                    setShowPostPages(currentPage - 5);
+                }
+                if ((currentPage - 1) % 5 === 0) {
+                // arrow right
+                    setShowPostPages(currentPage - 1);
+                }
+            }
+        }
+    }, [currentPage, postPages]);
     
     return(
         <div className={style.container}>
-            {alert("구현 준비중입니다.")}
+            {/* {alert("구현 준비중입니다.")} */}
             <div className={style.info}>
                 <img className={style.img} src={profileImage} alt=""  />
                 <p className={style.nickname}>
@@ -36,11 +140,11 @@ const MyReviewsDetail = () => {
             <span className={style.reviews_title}>
                 내가 쓴 글
             <div className={style.box}>
-            {filterData ? (
+            {postItems ? (
                     <>
-                        {filterData.map((filter) => (
+                        {postItems.map((filter) => (
                             <>
-                                <span className={style.mydata} key={filter}>
+                                <span className={style.mydata} key={filter.id}>
                                     <a className={style.goReview} href={`${process.env.REACT_APP_CLIENT_URL}/foryouview/:${filter.id}`}>{filter.title}</a>
                                 </span>
                                 <a className={style.goReview} href={`${process.env.REACT_APP_CLIENT_URL}/foryouview/:${filter.id}`}>
@@ -55,6 +159,48 @@ const MyReviewsDetail = () => {
                     <p className={style.empty_reviewsbox}>작성한 리뷰가 없습니다.</p>
                 )}
             </div>
+            <>
+                {!postPages ? null : (
+                    <div className={style.pageNationWrap}>
+                    <div className={style.arrowBox}>
+                        <a className={style.arrow}>
+                        <img
+                            src="/image/arrow_left_previous_icon.png"
+                            name="left"
+                            onClick={(e) => {
+                            pageArrowOnclickFn(e);
+                            }}
+                        />
+                        </a>
+                    </div>
+                    <ul>
+                        {isActive.map((el, index) => {
+                        if (index >= showPostPages && index <= showPostPages + 4) {
+                            return (
+                            <PageNationReviewButton
+                                key={index}
+                                index={index}
+                                isActive={el}
+                                pageNumberOnclickFn={pageNumberOnclickFn}
+                            />
+                            );
+                        }
+                        })}
+                    </ul>
+                    <div className={style.arrowBox}>
+                        <a className={style.arrow}>
+                        <img
+                            src="/image/arrow_right_next_icon.png"
+                            name="right"
+                            onClick={(e) => {
+                            pageArrowOnclickFn(e);
+                            }}
+                        />
+                        </a>
+                    </div>
+                    </div>
+                )}
+                </>
             </span>
         </div>
     )
